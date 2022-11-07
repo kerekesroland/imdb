4da446b99d7e9fc54ccafdd1d34b2c2a09c4f6d5
kerekesroland <kerekes98roland@gmail.com>
2022-11-07 18:59:30 +0100

    chore: initialized Navbar

---
+++ src/components/Navbar/Navbar.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import styles from "./Navbar.module.css";
+
+type Props = {};
+
+const Navbar = (props: Props) => {
+  return (
+    <div className={styles.nav}>
+      <ul></ul>
+    </div>
+  );
+};
+
+export default Navbar;
