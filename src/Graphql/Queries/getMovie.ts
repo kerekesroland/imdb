895a6c72e30c1d72abb16eb706bd02aab0932ae1
kerekesroland <kerekes98roland@gmail.com>
2022-11-11 18:14:23 +0100

    feat: GetMovie gql query

---
+++ src/Graphql/Queries/getMovie.ts
@@ -0,0 +1,37 @@
+import { gql } from "@apollo/client";
+
+export const MOVIE = gql`
+  query getMovie($id: ID!) {
+    movie(id: $id) {
+      id
+      name
+      overview
+      poster {
+        original
+      }
+      cast(limit: 5) {
+        id
+        person {
+          name
+        }
+        role {
+          ... on Cast {
+            character
+          }
+        }
+      }
+      crew(limit: 5) {
+        id
+        person {
+          name
+        }
+        role {
+          ... on Crew {
+            job
+            department
+          }
+        }
+      }
+    }
+  }
+`;
