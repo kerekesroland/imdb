82afa2eece4dea4ee4574490813018b5fb5060a5
kerekesroland <kerekes98roland@gmail.com>
2022-11-12 20:55:46 +0100

    update: updated the MovieCard component with responsiveness

--- src/components/MovieCard/MovieCard.tsx
+++ src/components/MovieCard/MovieCard.tsx
@@ -1,6 +1,7 @@
 import React, { FC } from "react";
 import { Link } from "react-router-dom";
 import styles from "./MovieCard.module.css";
+import NoImage from "../../assets/noImage.svg";
 
 type Category = {
   id: number;
@@ -16,24 +17,43 @@ type Props = {
 };
 
 const MovieCard: FC<Props> = ({ id, url, title, category, score }) => {
+  const firstThreeCategories = category.slice(0, 3);
   return (
     <Link className={styles.link} to={`/movie/${id}`}>
       <div className={styles.card}>
-        <img className={styles.img} src={url} alt="" />
+        <img
+          className={url ? styles.img : styles.noImg}
+          src={url ? url : NoImage}
+          alt=""
+        />
+
         <div className={styles.details}>
           <div className={styles.mainDetails}>
-            <p className={styles.title}>{title}</p>
+            <p className={styles.title}>
+              {title?.length > 30 ? `${title?.substring(0, 30)}...` : title}
+            </p>
             <span className={styles.scoreValue}>
               {score.toFixed(2)} <span className={styles.score}>/10</span>
             </span>
           </div>
           <div className={styles.categories}>
-            {category.map((c) => (
-              <p key={c.id} className={styles.category}>
-                {c.name}
-              </p>
-            ))}
+            {firstThreeCategories.length > 0 ? (
+              firstThreeCategories.map((c) => (
+                <p key={c.id} className={styles.category}>
+                  {c.name}
+                </p>
+              ))
+            ) : (
+              <p className={styles.category}>No categories</p>
+            )}
           </div>
+        </div>
+        <div className={styles.descriptionContainer}>
+          <p className={styles.description}>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Provident
+            autem eos saepe, vero quisquam eaque ad cum neque quod voluptatibus
+            id deserunt mollitia? Enim quia natus a, tempora earum mollitia!
+          </p>
         </div>
       </div>
     </Link>
