2a7c71afcb2eb60419d4198597f6b0446d9e741a
kerekesroland <kerekes98roland@gmail.com>
2022-11-12 20:55:21 +0100

    feat(component): Movie component added

--- src/components/Movie/Movie.tsx
+++ src/components/Movie/Movie.tsx
@@ -2,6 +2,11 @@ import styles from "./Movie.module.css";
 import { useQuery } from "@apollo/client";
 import { MOVIE } from "../../Graphql/Queries/getMovie";
 import { useParams } from "react-router-dom";
+import { Navbar } from "../Navbar/Navbar";
+import LoadingBubbles from "../LoadingSpinner/LoadingBubbles";
+import CategoryChip from "../CategoryChip/CategoryChip";
+import { ICategory } from "../../models/ICategory";
+import Overview from "../Overview/Overview";
 
 const Movie = () => {
   const { id } = useParams();
@@ -9,12 +14,40 @@ const Movie = () => {
     variables: { id },
   });
 
-  return (
-    <div className={styles.background}>
-      <div className={styles.card}>
-        <div className={styles.bg}></div>
+  if (loading) {
+    return (
+      <div className={styles.loader}>
+        <LoadingBubbles />
       </div>
-    </div>
+    );
+  }
+
+  if (error) {
+    return <h1>{error.extraInfo}</h1>;
+  }
+
+  return (
+    <>
+      <Navbar handleSearch={undefined} />
+      <div className={styles.container}>
+        <div className={styles.title__container}>
+          <h1 className={styles.title}>{data?.movie?.name}</h1>
+        </div>
+        <div className={styles.details}>
+          <div className={styles.image__container}>
+            <img src={data.movie.poster.original} alt="" loading="lazy" />
+          </div>
+          <div className={styles.categories}>
+            {data?.movie?.genres.map((category: ICategory) => (
+              <CategoryChip name={category.name} id={category.id} />
+            ))}
+          </div>
+          <div className={styles.overview__container}>
+            <Overview overview={data.movie.overview} />
+          </div>
+        </div>
+      </div>
+    </>
   );
 };
 
