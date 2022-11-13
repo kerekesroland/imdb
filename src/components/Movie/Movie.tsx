842d6d00b4269b899661af7f127a44c89675d1ab
kerekesroland <kerekes98roland@gmail.com>
2022-11-13 22:10:58 +0100

    feat: added category chip and the wikipedia url setter for the wiki link, also added some styling for the loader

--- src/components/Movie/Movie.tsx
+++ src/components/Movie/Movie.tsx
@@ -7,6 +7,9 @@ import LoadingBubbles from "../LoadingSpinner/LoadingBubbles";
 import CategoryChip from "../CategoryChip/CategoryChip";
 import { ICategory } from "../../models/ICategory";
 import Overview from "../Overview/Overview";
+import { Button } from "@mui/material";
+import { useEffect, useState } from "react";
+import { getWikiSearch } from "../../constants/wikipedia_endpoint";
 
 const Movie = () => {
   const { id } = useParams();
@@ -14,6 +17,20 @@ const Movie = () => {
     variables: { id },
   });
 
+  //wiki url for the wiki button
+  const [wikiUrl, setWikiUrl] = useState("");
+
+  //Set the wiki url so it can be used on the button
+  useEffect(() => {
+    const handleWikiSearch = async () => {
+      const search: any = await getWikiSearch(data?.movie?.name);
+      const url = `https://en.wikipedia.org/?curid=${search.query.search[0].pageid}`;
+      setWikiUrl(url);
+    };
+    handleWikiSearch();
+  }, [data?.movie?.name]);
+
+  //Checking if the data is still loading
   if (loading) {
     return (
       <div className={styles.loader}>
@@ -22,6 +39,7 @@ const Movie = () => {
     );
   }
 
+  //Checking if there as any error while loading
   if (error) {
     return <h1>{error.extraInfo}</h1>;
   }
@@ -39,11 +57,23 @@ const Movie = () => {
           </div>
           <div className={styles.categories}>
             {data?.movie?.genres.map((category: ICategory) => (
-              <CategoryChip name={category.name} id={category.id} />
+              <CategoryChip
+                key={category.id}
+                name={category.name}
+                id={category.id}
+              />
             ))}
           </div>
           <div className={styles.overview__container}>
             <Overview overview={data.movie.overview} />
+            <Button
+              variant="contained"
+              href={wikiUrl}
+              rel="noreferrer"
+              target={"_blank"}
+            >
+              Wikipedia
+            </Button>
           </div>
         </div>
       </div>
