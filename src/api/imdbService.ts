1f5ff74a0c9ba9d18c490e2a87ae9703a4c5e929
kerekesroland <kerekes98roland@gmail.com>
2022-11-15 17:42:01 +0100

    feat: changed the old imdb rapid api to omdbapi for more daily request

--- src/api/imdbService.ts
+++ src/api/imdbService.ts
@@ -1,18 +1,13 @@
 import axios from "axios";
 
-const options = {
-  method: "GET",
-  url: "https://movie-database-alternative.p.rapidapi.com/",
-  params: { s: "", r: "json", page: "1" },
-  headers: {
-    "X-RapidAPI-Key": "",
-    "X-RapidAPI-Host": "movie-database-alternative.p.rapidapi.com",
-  },
-};
-
 export const makeIMDBUrl = async (title: string) => {
-  options.params.s = title;
-  options.headers["X-RapidAPI-Key"] = `${process.env.REACT_APP_IMDB_KEY}`;
-  const res = await axios.request(options);
+  let res;
+  const searchRegExp = /\s/g;
+  const replaceWith = "\\";
+  const formattedTitle = title?.replace(searchRegExp, replaceWith);
+  res = await axios.get(
+    `http://www.omdbapi.com/?apikey=${process.env.REACT_APP_IMDB_KEY}&s=${formattedTitle}`
+  );
+
   return res.data;
 };
